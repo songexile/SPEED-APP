f53f72fee54fa9df5c59d2f756a2efcf5de08797
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-07 19:50:58 +1200

    connect to MongoDB

--- speed-backend/src/app.module.ts
+++ speed-backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Load environment variables
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URI, // Use the environment variable here
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
