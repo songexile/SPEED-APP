ff9a3509731a9b95a14e87d0aadd6baab1b69be5
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-14 18:16:12 +1200

    further setup eslint and remove redundant .json file

--- speed-frontend/.eslintrc.js
+++ speed-frontend/.eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
-  extends: 'react-app',
+  root: true,
+  extends: ['react-app', 'next/core-web-vitals'],
+  plugins: ['testing-library'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
   },
+  overrides: [
+    // Only uses Testing Library lint rules in test files
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react'],
+    },
+  ],
 }
