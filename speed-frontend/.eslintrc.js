0d42ee89fa75e469e6d47ccd4b0f280df61c9338
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-14 18:58:12 +1200

    fix jest tsconfig

--- speed-frontend/.eslintrc.js
+++ speed-frontend/.eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   root: true,
-  extends: ['react-app', 'next/core-web-vitals'],
+  extends: ['react-app', 'next', 'next/core-web-vitals', 'prettier'],
   plugins: ['testing-library'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': 'off',
   },
   overrides: [
-    // Only uses Testing Library lint rules in test files
+    // Only use Testing Library lint rules in test files
     {
       files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
       extends: ['plugin:testing-library/react'],
