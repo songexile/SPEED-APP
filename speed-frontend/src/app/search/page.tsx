8a237dff0c52560c9f609a4055a0adc7f6b93700
songexile <therunningbeta@gmail.com>
2023-09-24 20:53:08 +1300

    Created the search page, no content added, just boiler plate

---
+++ speed-frontend/src/app/search/page.tsx
@@ -0,0 +1,13 @@
+import Nav from '@/components/Nav'
+const SearchPage = () => {
+  return (
+    <div>
+      <div>
+        {' '}
+        <Nav />
+      </div>
+    </div>
+  )
+}
+
+export default SearchPage
