3fffa16d3ddea7188178252ef180ccc9337cfd84
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-25 01:17:23 +1300

    Refactor file structure and add .gitkeep files

--- speed-frontend/__tests__/pages/page.test.tsx
+++ speed-frontend/__tests__/pages/page.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import Home from './page'
+import Home from '../../src/app/page'
 
 test('renders welcome message and buttons', () => {
   render(<Home />) // Use 'render' directly without destructuring
