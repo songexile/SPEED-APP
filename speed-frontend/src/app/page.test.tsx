c5f0f4524223aee6c23d2239c399f9e1cd20cea7
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-25 00:24:29 +1300

    Update frontend workflow and test cases

--- speed-frontend/src/app/page.test.tsx
+++ speed-frontend/src/app/page.test.tsx
@@ -2,10 +2,31 @@ import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import Home from './page'
 
-describe('Home Component', () => {
-  it('renders the component', () => {
-    render(<Home />)
-    const mainElement = screen.getByRole('main')
-    expect(mainElement).toBeInTheDocument()
+test('renders welcome message and buttons', () => {
+  render(<Home />) // Use 'render' directly without destructuring
+
+  // Check if the welcome message is rendered
+  const welcomeMessage = screen.getByText('Welcome to SPEED!')
+  expect(welcomeMessage).toBeInTheDocument()
+
+  // Check if the "Login" button is rendered
+  const loginButton = screen.getByText('Login 🔍')
+  expect(loginButton).toBeInTheDocument()
+
+  // Check if the "Create an Account" button is rendered
+  const createAccountButton = screen.getByText('Create an Account ⭐')
+  expect(createAccountButton).toBeInTheDocument()
+})
+
+test('renders software engineering claim span', () => {
+  render(<Home />) // Use 'render' directly without destructuring
+
+  // Use a custom text matcher function to search for a substring of the text
+  const claimSpan = screen.getByText((content, element) => {
+    // Use a regular expression to match a substring of the text
+    const regex = /software engineering claims/
+    return regex.test(content)
   })
+
+  expect(claimSpan).toBeInTheDocument()
 })
--- speed-frontend/src/app/page.tsx
+++ speed-frontend/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import Nav from '../components/Nav'
 
 export default function Home() {
