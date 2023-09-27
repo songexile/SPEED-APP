b0db2611d88f98e81c7accdb70b50387e3e97d49
songexile <therunningbeta@gmail.com>
2023-09-28 12:07:27 +1300

    Submit now submits to database

--- speed-frontend/src/pages/submit/index.tsx
+++ speed-frontend/src/pages/submit/index.tsx
@@ -1,38 +1,91 @@
 import Nav from '@/components/Nav'
+import { useState } from 'react'
 
 const SubmitPage = () => {
+  const [formData, setFormData] = useState({
+    title: '',
+    authors: '',
+    journal: '',
+    year: '',
+    volume: '',
+    pages: '',
+    doi: '',
+  })
+
+  const handleChange = (e) => {
+    const { name, value } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const submitToDb = () => {
+    fetch('http://localhost:3001/submissions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+    console.log('Submitted')
+  }
   return (
     <div className="bg-base-100 flex flex-col items-center justify-center min-h-screen">
       <div className="flex flex-col space-y-4">
-        <input type="text" placeholder="Title" className="input input-bordered w-full max-w-xs" />
         <input
           type="text"
+          name="title"
+          placeholder="Title"
+          onChange={handleChange}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          name="authors"
           placeholder="Author(s)"
+          onChange={handleChange}
           className="input input-bordered w-full max-w-xs"
         />
         <input
           type="text"
+          name="journal"
           placeholder="Journal Name"
-          className="input input-bordered w-full max-w-xs"
-        />
-        <input
-          type="text"
-          placeholder="Year of publication"
-          className="input input-bordered w-full max-w-xs"
-        />
-        <input
-          type="text"
-          placeholder="Volume (Optional)"
+          onChange={handleChange}
           className="input input-bordered w-full max-w-xs"
         />
 
         <input
           type="text"
-          placeholder="Pages (Optional)"
+          name="year"
+          placeholder="Year of publication"
+          onChange={handleChange}
           className="input input-bordered w-full max-w-xs"
         />
-        <input type="text" placeholder="DOI" className="input input-bordered w-full max-w-xs" />
-        <button className="btn btn-primary">Submit</button>
+        <input
+          type="text"
+          name="volume"
+          placeholder="Volume (Optional)"
+          onChange={handleChange}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          name="pages"
+          placeholder="Pages (Optional)"
+          onChange={handleChange}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <input
+          type="text"
+          name="doi"
+          placeholder="DOI"
+          onChange={handleChange}
+          className="input input-bordered w-full max-w-xs"
+        />
+        <button onClick={submitToDb} className="btn btn-primary">
+          Submit
+        </button>
       </div>
       <Nav />
     </div>
