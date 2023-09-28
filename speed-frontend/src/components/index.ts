2d913078ddc69e7829393ce4c9dd7439d7dbd5cb
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-29 03:13:55 +1300

    Add CustomButtonIcon and CustomNavButtons components

---
+++ speed-frontend/src/components/index.ts
@@ -0,0 +1,7 @@
+import CustomButtonIcon from "./CustomButtonIcon";
+import CustomNavButtons from "./CustomNavButtons";
+
+export {
+  CustomNavButtons,
+  CustomButtonIcon
+}
--- speed-frontend/src/types/index.ts
+++ speed-frontend/src/types/index.ts
@@ -3,3 +3,15 @@ export interface IMetaProps {
   description: string
   canonical?: string
 }
+
+export interface CustomNavButtonsProps {
+  href: string;
+  icon: JSX.Element;
+  label: string;
+}
+
+
+export interface IconProps {
+  path: string;
+  className: string;
+}
