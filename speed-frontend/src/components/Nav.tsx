da1253633a5fb00d77d2f544ca3d8cf78a64e95b
songexile <therunningbeta@gmail.com>
2023-09-28 20:03:59 +1300

    Added page navigation

--- speed-frontend/src/components/Nav.tsx
+++ speed-frontend/src/components/Nav.tsx
@@ -16,9 +16,11 @@ const Nav = () => {
             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
           />
         </svg>
-        <span className="btm-nav-label">Search</span>
+        <a href="/search">
+          <span className="btm-nav-label">Search</span>
+        </a>
       </button>
-      <button className="active bg-primary text-accent border-blue-600">
+      <button className=" bg-primary text-accent ">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-5 w-5"
@@ -33,7 +35,9 @@ const Nav = () => {
             d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <span className="btm-nav-label">Submit </span>
+        <a href="/submit">
+          <span className="btm-nav-label">Submit</span>
+        </a>
       </button>
       <button className="bg-primary primary-focus  text-accent">
         <svg
@@ -50,7 +54,9 @@ const Nav = () => {
             d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
           />
         </svg>
-        <span className="btm-nav-label">Analyst </span>
+        <a href="/analyst">
+          <span className="btm-nav-label">Analyst</span>
+        </a>
       </button>
       <button className="bg-primary text-accent">
         <svg
@@ -67,7 +73,9 @@ const Nav = () => {
           <line x1="12" y1="8" x2="12" y2="12"></line>
           <line x1="12" y1="16" x2="12" y2="16"></line>
         </svg>
-        <span className="btm-nav-label">Moderator</span>
+        <a href="/moderator">
+          <span className="btm-nav-label">Moderator</span>
+        </a>
       </button>
       <button className="bg-primary text-accent">
         <svg
@@ -98,7 +106,9 @@ const Nav = () => {
           <circle cx="12" cy="12" r="10"></circle>
           <path d="M4 12l2.5 2.5L9 12M15 12l-2.5 2.5L9 12m6 4h2M4 8h16M4 16h16"></path>
         </svg>
-        <span className="btm-nav-label">Admin</span>
+        <a href="/admin">
+          <span className="btm-nav-label">Admin</span>
+        </a>
       </button>
     </div>
   )
