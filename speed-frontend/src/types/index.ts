05e9e51dcd0af15aa699b76ab75cbf17b4ab8ce6
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-29 03:37:10 +1300

    Add Errors and FormData types in index.ts

--- speed-frontend/src/types/index.ts
+++ speed-frontend/src/types/index.ts
@@ -4,12 +4,31 @@ export interface IMetaProps {
   canonical?: string
 }
 
+export type Errors = {
+  title?: string
+  authors?: string
+  journal?: string
+  year?: string
+  volume?: string
+  pages?: string
+  doi?: string
+}
+
+export type FormData = {
+  title: string
+  authors: string
+  journal: string
+  year: string
+  volume: string
+  pages: string
+  doi: string
+}
+
 export interface CustomNavButtonsProps {
   href: string;
   icon: JSX.Element;
   label: string;
 }
-
 
 export interface IconProps {
   path: string;
