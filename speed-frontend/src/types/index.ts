987ca8dd23d1256d62aeb509e9bec44cb7162935
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-09-29 01:41:19 +1300

    Refactor types, enhance API error handling

--- speed-frontend/src/types/index.ts
+++ speed-frontend/src/types/index.ts
@@ -3,3 +3,23 @@ export interface IMetaProps {
   description: string
   canonical?: string
 }
+
+export type Errors = {
+  title?: string
+  authors?: string
+  journal?: string
+  year?: string
+  volume?: string
+  pages?: string
+  doi?: string
+}
+
+export type FormData = {
+  title: string
+  authors: string
+  journal: string
+  year: string
+  volume: string
+  pages: string
+  doi: string
+}
