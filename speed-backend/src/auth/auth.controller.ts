ba4f4664448eacc2fd10b9e3b2624f9d6fd74cff
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-10-02 03:21:34 +1300

    Add login and signup functionality with JWT auth

--- speed-backend/src/auth/auth.controller.ts
+++ speed-backend/src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { SignUpDto } from './dto/signup.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('/signup')
+    signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+        return this.authService.signUp(signUpDto);
+    }
+
+    @Post('/login')
+    login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+        return this.authService.login(loginDto);
+    }
+}
