ba54f21ae46ce61bc8d4259afbbb887afd743ca6
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-10-03 11:24:38 +1300

    Remove logOut button from Nav component

--- speed-frontend/src/components/Nav.tsx
+++ speed-frontend/src/components/Nav.tsx
@@ -15,8 +15,6 @@ const Nav = () => {
   const moderatorIconPath =
     'M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z'
 
-  const logOutIconPath = 'M11 17l-5-5m0 0l5-5m-5 5h12'
-
   const adminIconPath =
     'M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z'
 
@@ -51,12 +49,6 @@ const Nav = () => {
         icon={<CustomButtonIcon path={moderatorIconPath} className="ml-7" />}
         label="Moderator"
       />
-
-      <button className="bg-primary text-accent">
-        <CustomButtonIcon path={logOutIconPath} className="ml-[-5px]" />
-
-        <span className="btm-nav-label">Log Out</span>
-      </button>
 
       <CustomNavButtons
         href="/admin"
