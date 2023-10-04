0dbddcf53fb4d0f5288701adefc17aeea7d9ba95
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-10-04 23:53:57 +1300

    Add some back end test

--- speed-backend/src/submissions/submissions.controller.spec.ts
+++ speed-backend/src/submissions/submissions.controller.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SubmissionsController } from './submissions.controller';
+import { SubmissionsService } from './submissions.service';
+
+// Mock the SubmissionModel
+const submissionModelMock = {
+  // Define mock methods or properties here as needed.
+};
 
 describe('SubmissionsController', () => {
   let controller: SubmissionsController;
@@ -7,6 +13,10 @@ describe('SubmissionsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SubmissionsController],
+      providers: [
+        SubmissionsService,
+        { provide: 'SubmissionModel', useValue: submissionModelMock }, // Mock SubmissionModel
+      ],
     }).compile();
 
     controller = module.get<SubmissionsController>(SubmissionsController);
