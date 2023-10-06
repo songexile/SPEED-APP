a8799c93d02b8f4c30bd24fc76f09702aee3426b
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-10-06 14:44:19 +1300

    resolve conflict with main branch

---
+++ speed-backend/src/submissions/submissions.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { SubmissionsService } from './submissions.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+describe('SubmissionsService', () => {
+    let service: SubmissionsService;
+    let model: Model<any>;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                SubmissionsService,
+                {
+                    provide: getModelToken('Submission'), // Use the correct model token
+                    useValue: Model,
+                },
+            ],
+        }).compile();
+
+        service = module.get<SubmissionsService>(SubmissionsService);
+        // model = module.get<Model<any>>(getModelToken('Submission'));
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+});
