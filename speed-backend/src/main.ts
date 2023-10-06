f2f60ecde537586a32510a7015aa60a52bcfc473
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-10-06 17:26:13 +1300

    Enable CORS with specific origins and methods

--- speed-backend/src/main.ts
+++ speed-backend/src/main.ts
@@ -5,7 +5,12 @@ import { ConfigModule } from '@nestjs/config';
 async function bootstrap() {
   ConfigModule.forRoot() // Load environment variables
   const app = await NestFactory.create(AppModule, { cors: true });
-  // app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://next-gen.miguelemmara.me'], // Add all allowed origins here
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(process.env.PORT, () => console.log(`\nApp listen at http://localhost:${process.env.PORT}/`));
 }
 bootstrap();
