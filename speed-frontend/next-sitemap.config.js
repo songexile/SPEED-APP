42cf900991bc661c4e239c3411a129454f8d808b
Miguel Emmara <71995037+MiguelEmmara-ai@users.noreply.github.com>
2023-10-18 21:46:49 +1300

    Improve SEO

---
+++ speed-frontend/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://next-gen.miguelemmara.me/',
+  // generateRobotsTxt: true, // (optional)
+  // ...other options
+}
